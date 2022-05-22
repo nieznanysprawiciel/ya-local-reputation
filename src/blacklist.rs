22e0c57ff3e55653b8b53a26d41ff558216dad78
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2022-05-22 13:42:03 +0200

    Working blacklist

--- src/blacklist.rs
+++ src/blacklist.rs
@@ -53,10 +53,20 @@ impl NegotiatorConstructor<BlacklistReputation> for BlacklistReputation {
         let config: BlacklistReputationsConfig = serde_yaml::from_value(config)?;
         let runtime = Runtime::new()?;
 
+        flexi_logger::Logger::try_with_env()?
+            .log_to_file(
+                flexi_logger::FileSpec::default()
+                    .directory(&working_dir)
+                    .basename("blacklist"),
+            )
+            .start()?;
+
         let blacklist = match fs::read_to_string(working_dir.join("blacklist.yaml")) {
             Ok(content) => serde_yaml::from_str(&content)?,
             Err(_) => vec![],
         };
+
+        log::info!("Starting BlacklistReputation plugin.");
 
         Ok(BlacklistReputation {
             config: Arc::new(config),
@@ -80,6 +90,7 @@ impl Drop for BlacklistReputation {
                 .drain(..)
                 .collect::<Vec<NodeId>>()
         };
+
         if let Ok(content) = serde_yaml::to_string(&blacklist) {
             fs::write(self.workdir.join("blacklist.yaml"), content).ok();
         }
@@ -101,11 +112,15 @@ impl NegotiatorComponent for BlacklistReputation {
             .blacklist
             .contains(&demand.issuer)
         {
+            log::info!("Rejecting blacklisted node: {}", demand.issuer);
+
             return Ok(NegotiationResult::Reject {
-                reason: RejectReason::new("Node is blacklisted die to not paying Invoices."),
+                reason: RejectReason::new("Node is blacklisted due to not paying Invoices."),
                 is_final: true,
             });
         }
+
+        log::debug!("Node {} allowed (not blacklisted).", demand.issuer);
         Ok(NegotiationResult::Ready {
             proposal: offer,
             score,
@@ -119,33 +134,48 @@ impl NegotiatorComponent for BlacklistReputation {
         agreement_id: &str,
         _result: &AgreementResult,
     ) -> anyhow::Result<()> {
-        {
-            let mut state = self.state.lock().unwrap();
-            if let Some(record) = state.agreements.get_mut(agreement_id) {
-                let now = Instant::now();
-                let state = self.state.clone();
-                let deadline = now + self.config.payment_timeout;
-                let agreement_id = agreement_id.to_string();
+        log::trace!("on_agreement_terminated [{}]", agreement_id);
 
-                record.terminated = Some(now);
+        let mut state = self.state.lock().unwrap();
+        if let Some(record) = state.agreements.get_mut(agreement_id) {
+            let now = Instant::now();
+            let state = self.state.clone();
+            let deadline = now + self.config.payment_timeout;
+            let agreement_id = agreement_id.to_string();
 
-                self.runtime.spawn(async move {
-                    tokio::time::sleep_until(deadline).await;
+            record.terminated = Some(now);
+            let node_id = record.node;
 
-                    let mut state = state.lock().unwrap();
+            log::debug!(
+                "Setting timer ({}) for agreement [{}], node: {}",
+                humantime::format_duration(self.config.payment_timeout),
+                agreement_id,
+                node_id
+            );
 
-                    // If we don't find Agreement in the map, it have been paid.
-                    if let Some(record) = state.agreements.remove(&agreement_id) {
-                        state.blacklist.push(record.node);
-                    }
-                });
-            }
-            Ok(())
+            self.runtime.spawn(async move {
+                tokio::time::sleep_until(deadline).await;
+
+                let mut state = state.lock().unwrap();
+
+                // If we don't find Agreement in the map, it have been paid.
+                if let Some(record) = state.agreements.remove(&agreement_id) {
+                    log::info!(
+                        "Node [{}] didn't pay agreement [{}]. Blacklisting..",
+                        node_id,
+                        agreement_id
+                    );
+                    state.blacklist.push(record.node);
+                }
+            });
         }
+        Ok(())
     }
 
     /// Store `Agreement` information and track it's state.
     fn on_agreement_approved(&mut self, agreement: &AgreementView) -> anyhow::Result<()> {
+        log::trace!("on_agreement_approved [{}]", agreement.id);
+
         let record = TrackedAgreement {
             id: agreement.id.clone(),
             node: agreement.requestor_id()?,
@@ -161,6 +191,12 @@ impl NegotiatorComponent for BlacklistReputation {
                 .unwrap()
                 .agreements
                 .insert(agreement.id.clone(), record);
+
+            log::info!(
+                "Registered agreement [{}] for node [{}].",
+                agreement.id,
+                agreement.requestor_id()?
+            );
             Ok(())
         }
     }
@@ -172,14 +208,27 @@ impl NegotiatorComponent for BlacklistReputation {
         agreement_id: &str,
         event: &AgreementEvent,
     ) -> anyhow::Result<()> {
+        log::trace!("on_agreement_event [{}]", agreement_id);
+
         let mut state = self.state.lock().unwrap();
         match event {
             AgreementEvent::InvoicePaid => {
-                state.agreements.remove(agreement_id);
+                if let Some(record) = state.agreements.remove(agreement_id) {
+                    log::info!(
+                        "Node [{}] paid invoice for agreement [{}]. Stop tracking..",
+                        record.node,
+                        agreement_id
+                    );
+                }
                 Ok(())
             }
             AgreementEvent::InvoiceRejected => {
                 if let Some(record) = state.agreements.remove(agreement_id) {
+                    log::info!(
+                        "Node [{}] rejected invoice for agreement [{}]. Blacklisting..",
+                        record.node,
+                        agreement_id
+                    );
                     state.blacklist.push(record.node)
                 }
                 Ok(())
